e08abff2fc8756bad0b78edd575921c20d290f63
Makkiiew <chisholmmmm@gmail.com>
2026-04-13 17:55:57 +0900

    C: add verboos

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import os
 from dotenv import load_dotenv
 from google import genai
-#import lmstudio as lms
+from google.genai import types
+import argparse
+
 
 
 
 def main():
+    parser = argparse.ArgumentParser(description="chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")    
+    args = parser.parse_args()
+
+
     load_dotenv()
 
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -13,24 +21,29 @@ def main():
        raise RuntimeError("GEMINI_API_KEY not found in environment variables. Check your .env file!")
 
     client = genai.Client(api_key=api_key)
-    
 
-
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    messages = [
+        types.Content(
+            role="user", 
+            parts=[types.Part(text=args.user_prompt)]
+        )
+    ]
 
     try:
         response = client.models.generate_content(
-#            model = lms.llm(),
-            model="gemini-2.5-flash",
-            contents=prompt
+            model="gemini-2.5-flash-lite",
+            contents=messages
         )
+
         if response.usage_metadata is None:
             raise RuntimeError("API request failed: No usage metadata returned.")
         if not response.usage_metadata:
             raise RuntimeError("Gemini API response appears to be malformed")
 
-        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-        print("Response tokens:", response.usage_metadata.candidates_token_count)
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
         print(response.text)
     except Exception as e:
         print(f"Error: {e}")
